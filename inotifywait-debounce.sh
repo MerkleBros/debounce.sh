644f481591c2a169080079256758298ec4d644d2
Patrick McCarver <patrick@merklebros.com>
2020-05-09 20:09:07 -0400

    Code review: readonly variables
    Make constants readonly and follow Google naming conventions.

--- inotifywait-debounce.sh
+++ inotifywait-debounce.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 set -Ceuo pipefail
 
-WATCH_FILE_PATH=$1
-DEBOUNCE_INTERVAL_SECONDS=$2
-DEBOUNCE_ACTION="${@:3}"
-DEBOUNCE_PID=""
+readonly WATCH_FILE_PATH="$1"
+readonly DEBOUNCE_INTERVAL_SECONDS="$2"
+readonly DEBOUNCE_ACTION="${*:3}"
+debounce_pid=""
 
 run_debounce_action() {
     echo "Waiting debounce interval to perform debounce action: ${DEBOUNCE_ACTION}"
@@ -19,12 +19,12 @@ inotifywait_with_debounce() {
         "$WATCH_FILE_PATH"
     do
         echo "FILE_SAVED at SECONDS: ${SECONDS}"
-        if test -n "$DEBOUNCE_PID" && ps -p $DEBOUNCE_PID > /dev/null ; then
-            echo "Killing debounce PID: ${DEBOUNCE_PID}"
-            kill $DEBOUNCE_PID
+        if test -n "$debounce_pid" && ps -p $debounce_pid > /dev/null ; then
+            echo "Killing debounce PID: ${debounce_pid}"
+            kill $debounce_pid
         fi
         run_debounce_action &
-        DEBOUNCE_PID=$!
+        debounce_pid=$!
     done
 }
 
